8704ba1752ce70395feab62e16abbc3a93ad0bcc
JohnnyDev2001 <johnny.devcode@gmail.com>
2020-07-14 06:53:44 -0300

    Configurações Iniciais

---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'mj',
+    database: 's_product',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+}
